d12d6e8699d4df5e0b1daa3188d6b52296dff8e5
Dmitri Grecov <vokerg@gmail.com>
2017-12-15 09:08:05 +0100

    adding services

--- src/App.js
+++ src/App.js
@@ -11,14 +11,48 @@ const persistedState = {shops: [
     name: "Shop1",
     address: "Norrebrogade 1",
     favorited: false,
-    description: "This is a long description supposed to be displayed on a shop page"
+    description: "This is a long description supposed to be displayed on a shop page",
+    services: [
+      "Washing", "Cutting"
+    ],
+    ratings: [
+      {
+        id: "1",
+        author: "John Lennon",
+        rating: "5",
+        comment: "Imagine all the people"
+      },
+      {
+        id: "2",
+        author: "Paul McCartney",
+        rating: "2",
+        comment: "I'm not dead"
+      },
+      {
+        id: "3",
+        author: "Ringo Starr",
+        rating: "1",
+        comment: "I'd like to be under the sea"
+      }
+    ]
   },
   {
     id: "2",
     name: "Shop2",
     address: "Norrebrogade 2",
     favorited: true,
-    description: "This is a long description supposed to be displayed on a shop page"
+    description: "This is a long description supposed to be displayed on a shop page",
+    services: [
+      "Shaving", "Washing", "Cutting", "Manicure"
+    ],
+    ratings: [
+      {
+        id: "4",
+        author: "Elton Jonh",
+        rating: "1",
+        comment: "Yellow brick wall"
+      }
+    ],
   },
 ]};
 
@@ -41,12 +75,14 @@ const shops = (state = [], action) => {
   }
 }
 
-
 const ShopList = (props) => {
+  console.log(props.route);
+  let shops = (props.shops === undefined ? store.getState().shops : props.shops);
   return (
     <div>
-      {store.getState().shops.map(element=>
+      {shops.map(element=>
           <ShopPreview
+            key={element.id}
             shop={element}
             onFavoriteClick= {
               () => {
@@ -66,7 +102,7 @@ const ShopPreview = (props) => {
   return (
       <div>
         <div>
-          <Link exact to={ "/shop/" + props.shop.id }>
+          <Link exact="true" to={ "/shop/" + props.shop.id }>
             { props.shop.name }
           </Link>
         </div>
@@ -78,7 +114,6 @@ const ShopPreview = (props) => {
 
 const store = createStore(combineReducers({shops}), persistedState);
 
-
 const getShopById = (shop_id) => {
   let shop_found;
   store.getState().shops.forEach(shop => {
@@ -89,8 +124,13 @@ const getShopById = (shop_id) => {
   return shop_found;
 };
 
+const getShopsByService = (service) => {
+  return store.getState().shops;
+};
+
 
 class Shop extends Component{
+  /*
   componentDidMount() {
     this.unsubscribe = store.subscribe(() => {
       this.forceUpdate();
@@ -99,17 +139,79 @@ class Shop extends Component{
   componentWillUnmount() {
     this.unsubscribe();
   }
+
+  */
   render() {
 
     let shop = getShopById(this.props.match.params.shop_id);
     return (
       <div>
-        <div>{ shop.name }</div>
-        <div>{ shop.address }</div>
-        <div>{ shop.description }</div>
+        <div>
+          <div>{ shop.name }</div>
+          <div>{ shop.address }</div>
+          <div>{ shop.description }</div>
+        </div>
+        <ShopServiceList services={ shop.services } />
+        <RatingList ratings={ shop.ratings } />
       </div>
     )
   }
+};
+
+const ShopServiceList = (props) => {
+  return (
+    <div>
+      <div>
+        Services:
+      </div>
+      {
+        props.services.map(service =>
+          <Link key={service} to={"/service/" + service}> { service + "  "}</Link>
+        )}
+    </div>
+  )
+};
+
+const ShopsByService = (props) => {
+  return (
+    <div>
+      <div>
+        { props.service }
+      </div>
+      <ShopList shops={ getShopsByService(props.service) } />
+    </div>
+  )
+}
+
+const RatingList = (props) => {
+  return (
+    <div>
+    <div>
+      Ratings:
+    </div>
+      { props.ratings.map((rating) =>
+          <Rating
+            key={rating.id}
+            rating={rating}
+          />
+      )}
+    </div>
+  )
+};
+
+const Rating = (props) => {
+  const { rating } = props;
+  return (
+    <div>
+      <div>
+        <span>Author: { rating.author }</span>
+        <span> Rating: { rating.rating }</span>
+      </div>
+      <div>
+        <span>Comment: { rating.comment }</span>
+      </div>
+    </div>
+  )
 };
 
 
@@ -132,8 +234,9 @@ class App extends Component {
         </header>
         <BrowserRouter>
           <div>
-            <Route exact path="/" component= {ShopList} />
+            <Route exact path="/" component= {ShopList} shops={store.getState().shops} />
             <Route exact path="/shop/:shop_id" component= {Shop} />
+            <Route exact path="/service/:service" component= {ShopsByService} />
           </div>
         </BrowserRouter>
       </div>
