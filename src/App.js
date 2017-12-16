07251485defb60ea885dffb64a68f7bab2320ba4
Dmitri Grecov <vokerg@gmail.com>
2017-12-16 15:09:37 +0100

    dealing with state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import logo from './logo.svg';
 
 import { createStore, combineReducers } from 'redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Provider, connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import './App.css';
 
 const persistedState = {shops: [
@@ -78,15 +79,15 @@ const shops = (state = [], action) => {
 
 const getShopsByFilter = (shops, filter = "all") => {
   switch (filter) {
-    case "all": {
-      return shops;
-    }
     case "favorites": {
       return shops.filter(shop => shop.favorited);
     }
+    case "all":
+    default: {
+      return shops;
+    }
   }
 }
-
 
 const Filter = () => {
   return (
@@ -104,61 +105,48 @@ const Filter = () => {
   )
 }
 
-
-const MainPageView = ({shops}) => {
+const MainPageView = ({shops, onFavoriteClick}) => {
   return (
     <div>
       <Filter />
-      <ShopList shops={shops} />
+      <ShopList shops={shops} onFavoriteClick={onFavoriteClick} />
     </div>
   )
 }
-const mapStateToProp = (state, ownProps) => {
-  const filter = ownProps.match.params.filter;
-  const shops = getShopsByFilter(state.shops, filter);
+const mapStateToProps = ({shops}, {match}) => {
+  const filter = match.params.filter;
+  const returnShops = getShopsByFilter(shops, filter)
   return {
-    shops
+    shops: returnShops
   }
 }
-const mapDispatchToProp = () => {
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFavoriteClick: (id) => {
+      dispatch({
+        type: "ADD_FAVORITE",
+        id: id
+      })
+    }
+  }
 }
-const MainPage = connect(mapStateToProp, mapDispatchToProp)(MainPageView);
-/*
-const MainPage = ({match}) => {
+const MainPage = connect(mapStateToProps, mapDispatchToProps)(MainPageView);
 
-
-  const filter = match.params.filter;
-
-  let shops = getShopsByFilter(store.getState().shops, filter);
-  return (
-    <MainPageView shops={shops}/>
-  )
-}
-*/
-const ShopList = ({shops}) => {
+const ShopList = ({shops, onFavoriteClick}) => {
   return (
     <div>
       {shops.map(element=>
-          <ShopPreview
-            key={element.id}
-            shop={element}
-            onFavoriteClick= {
-              () => {
-                store.dispatch({
-                  type: "ADD_FAVORITE",
-                  id: element.id
-                })
-              }
-            }
-          />
+        <ShopPreview
+          key={element.id}
+          shop={element}
+          onFavoriteClick= { ()=>{onFavoriteClick(element.id)} }
+        />
       )}
     </div>
   )
 }
 
-const ShopPreview = (props) => {
-  const {shop} = props;
+const ShopPreview = ({shop, onFavoriteClick}) => {
   return (
       <div>
         <div>
@@ -167,16 +155,14 @@ const ShopPreview = (props) => {
           </Link>
         </div>
         <div>{ shop.address }</div>
-        <button onClick= { props.onFavoriteClick }>{shop.favorited ? "Unfavorite" : "Favorite"}</button>
+        <button onClick= { onFavoriteClick }>{shop.favorited ? "Unfavorite" : "Favorite"}</button>
       </div>
   )
 };
 
-const store = createStore(combineReducers({shops}), persistedState);
-
-const getShopById = (shop_id) => {
+const getShopById = (shops, shop_id) => {
   let shop_found;
-  store.getState().shops.forEach(shop => {
+  shops.forEach(shop => {
     if (shop.id === shop_id) {
       shop_found=shop;
     }
@@ -184,75 +170,76 @@ const getShopById = (shop_id) => {
   return shop_found;
 };
 
-const getShopsByService = (service) => {
-  return store.getState().shops.filter(
-    shop => shop.services.includes(service)
+const getShopsByService = (shops, service) => shops.filter(
+  shop => shop.services.includes(service)
+);
+
+const SingleShop = (props) => {
+  return (
+    <div>
+      <h2>{ props.name }</h2>
+      <h3>{ props.address }</h3>
+      <h4>{ props.description }</h4>
+    </div>
   )
 };
-
-
-class Shop extends Component{
-  /*
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate();
-    });
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  */
-  render() {
-
-    let shop = getShopById(this.props.match.params.shop_id);
-    return (
+const ShopView = ({shop}) => {
+  return(
       <div>
-        <div>
-          <h2>{ shop.name }</h2>
-          <h3>{ shop.address }</h3>
-          <h4>{ shop.description }</h4>
-        </div>
+        <SingleShop name={shop.name} address={shop.address} description={shop.description}/>
         <ShopServiceList services={ shop.services } />
         <RatingList ratings={ shop.ratings } />
       </div>
-    )
-  }
+    );
 };
+const mapStateToPropShop = ({shops}, {match}) => {
+  const shop_id = match.params.shop_id
+  return {
+    shop: getShopById(shops, shop_id)
+  }
+}
+const Shop = withRouter(connect(mapStateToPropShop, ()=>({}))(ShopView));
 
-const ShopServiceList = (props) => {
+const ShopServiceList = ({services}) => {
   return (
     <div>
       <div>
         Services:
       </div>
       {
-        props.services.map(service =>
-          <Link key={service} to={"/service/" + service}> { service + "  "}</Link>
-        )}
+        services.map(service =>
+          <Link key={service} to={"/service/" + service}> { service + "  "}</Link>)
+      }
     </div>
   )
 };
 
-const ShopsByService = ({match}) => {
-  const service = match.params.service
+const ShopsByServiceView = ({service, shops}) => {
   return (
     <div>
       <div>
         { service }
       </div>
-      <ShopList shops={ getShopsByService(service) } />
+      <ShopList shops={ shops } />
     </div>
   )
 }
+const mapStateToPropService = ({shops}, {match}) => {
+  const service = match.params.service;
+  return {
+    service,
+    shops: getShopsByService(shops, service)
+  }
+};
+const ShopsByService = withRouter(connect(mapStateToPropService, ()=>({}))(ShopsByServiceView));
 
-const RatingList = (props) => {
+const RatingList = ({ratings}) => {
   return (
     <div>
     <div>
       Ratings:
     </div>
-      { props.ratings.map((rating) =>
+      { ratings.map((rating) =>
           <Rating
             key={rating.id}
             rating={rating}
@@ -277,40 +264,25 @@ const Rating = (props) => {
   )
 };
 
-
-class App extends Component {
-  componentDidMount() {
-    this.unsubscribe = store.subscribe (() => {
-      this.forceUpdate();
-    })
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Provider store= {store}>
-          <BrowserRouter>
-            <div>
-              <Route exact path="/" component= {MainPage} />
-              <Route exact path="/shop/:shop_id" component= {Shop} />
-              <Route exact path="/service/:service" component= {ShopsByService} />
-              <Route path="/filter/:filter" component= {MainPage} />
-            </div>
-          </BrowserRouter>
-        </Provider>
-      </div>
-    );
-  }
-}
-
-//store.dispatch({type:"DO_NOTHING"});
-//store.subscribe(App);
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <Provider store= {createStore(combineReducers({shops}), persistedState)}>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component= {MainPage} />
+            <Route exact path="/shop/:shop_id" component= {Shop} />
+            <Route exact path="/service/:service" component= {ShopsByService} />
+            <Route path="/filter/:filter" component= {MainPage} />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
