2a87c8b4bc736f3e80f5858928cebe9d51baee27
Dmitri Grecov <vokerg@gmail.com>
2017-12-17 21:50:14 +0100

    adjusting maps solution

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const GoogleMaps = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBt1T8DbzVsxQ9kpsXLeNvkSR9SOHZoxDU&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -14,12 +14,11 @@ const GoogleMaps = compose(
 )((props) =>
   <GoogleMap
     defaultZoom={11}
-    defaultCenter={{lat: 55.718035, lng: 12.470284}}
+    defaultCenter={props.markers[0]}
   >
   {props.markers.map(marker => {
-    console.log(marker)
     return (
-      <Marker position={marker} onClick={props.onMarkerClick} />
+      <Marker key={marker.lat} position={marker} onClick={() => {}} />
     )
   })}
   < /GoogleMap>
--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import ShopPreview from './ShopPreview'
+import GoogleMaps from './GoogleMaps'
 
 const ShopList = ({shops, onFavoriteClick}) => {
   return (
     <div>
-      {shops.map(element=>
-        <ShopPreview
-          key={element.id}
-          shop={element}
-          onFavoriteClick= { ()=>{onFavoriteClick(element.id)} }
-        />
-      )}
+      <div>
+        {shops.map(element=>
+          <ShopPreview
+            key={element.id}
+            shop={element}
+            onFavoriteClick= { ()=>{onFavoriteClick(element.id)} }
+          />
+        )}
+      </div>
+      <hr></hr>
+      <GoogleMaps markers={shops.map(
+        shop => shop.coordinates
+      )}/>
     </div>
   )
 }
