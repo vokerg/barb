d3138aa2ddff07488d60b4b9ec9110197439ebcb
Dmitri Grecov <vokerg@gmail.com>
2018-01-28 22:03:30 +0100

    filtering shop list on map resize

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -2,43 +2,90 @@ import React from 'react'
 import ShopPreview from './ShopPreview'
 import GoogleMaps from './GoogleMaps'
 
-const ShopList = ({shops, onFavoriteClick, isShopsRequested}) => {
-  let mapRef1
-  let mapBoundsChange=false
-  return (
-    <div>
-      <GoogleMaps markers={shops.map(
-        shop => shop.coordinates
-      )}
-      mapRef={map => mapRef1 = map}
-      onBoundsChanged={() => {
-        if (mapBoundsChange) {
-          return
+class ShopList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      visibleShops: [],
+      bounds: {
+        b: {
+          b: 0,
+          f: 0
+        },
+        f: {
+          b: 0,
+          f: 0
         }
-        mapBoundsChange = true
-        let bounds = mapRef1.getBounds()
-        setTimeout(() => {
-          mapBoundsChange = false;
-          if (mapRef1 !== null) {
-            console.log("coordinates from map", bounds)
-          }
-        }, 1000)
-
-      }}
-      />
-      <hr></hr>
-      {isShopsRequested ? <div>Loading...</div>:
-        <div>
-          {shops.map(element=>
-            <ShopPreview
-              key={element.id}
-              shop={element}
-              onFavoriteClick= { ()=>{onFavoriteClick(element.id)} }
-            />
-          )}
-        </div>
       }
-    </div>
-  )
+    }
+  }
+
+  filterShops(shop) {
+    let {lat, lng} = shop.coordinates
+    lat = parseFloat(lat)
+    lng = parseFloat(lng)
+    const {f, b} = this.state.bounds
+    return (!f || !b) || ((lat >= f.b) && (lat <=f.f) && (lng >= b.b) && (lng <= b.f))
+  }
+
+  setBoundsState(bounds) {
+    this.setState({
+      ...this.state,
+      bounds: {
+        b: {
+          b: bounds.b.b,
+          f: bounds.b.f
+        },
+        f: {
+          b: bounds.f.b,
+          f: bounds.f.f
+        }
+      }
+    })
+  }
+
+  render() {
+      const {shops, onFavoriteClick, isShopsRequested} = this.props
+      let mapRef1
+      let mapBoundsChange=false
+      let bounds
+      return (
+        <div>
+          <GoogleMaps markers={shops.map(
+            shop => shop.coordinates
+          )}
+          mapRef={map => mapRef1 = map}
+          onBoundsChanged={
+            () => {
+              bounds = mapRef1.getBounds()
+              if (mapBoundsChange) return
+              mapBoundsChange = true
+              setTimeout(() => {
+                mapBoundsChange = false;
+                this.setBoundsState(bounds)
+              }, 300)
+            }
+          }
+          />
+          <hr></hr>
+          {isShopsRequested ? <div>Loading...</div>:
+            <div>
+              {shops
+                .filter(this.filterShops.bind(this))
+                .map(element=>
+                  <ShopPreview
+                    key={element.id}
+                    shop={element}
+                    onFavoriteClick= { ()=>{onFavoriteClick(element.id)} }
+                  />
+                )
+              }
+            </div>
+          }
+        </div>
+      )
+  }
 }
+
+
 export default ShopList
